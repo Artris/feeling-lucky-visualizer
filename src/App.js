7917cedd922bf0b5d1a5fad235e3e0f9e2bf6e81
Alireza Alidousti <alireza.eva.u23@gmail.com>
2018-01-19 00:32:04 -0800

    processes raw data for visualization

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
+import config from './config';
 
 const spacing = 16;
 const style = {
@@ -24,9 +25,10 @@ const styles = theme => style;
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { width: 0, height: 0 };
+    this.state = { width: 0, height: 0, selected: this.props.data[0] };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     this.getComponentWidth = this.getComponentWidth.bind(this);
+    this.selectNode = this.selectNode.bind(this);
   }
 
   componentDidMount() {
@@ -42,8 +44,11 @@ class App extends Component {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
+  selectNode(index) {
+    this.setState({ selected: this.state.data[index] });
+  }
+
   getComponentWidth() {
-    console.log(this.state.width);
     if (this.state.width < 960) {
       const whiteSpaceWidth = (style.paper.margin + style.paper.padding) * 2;
       return this.state.width - whiteSpaceWidth;
@@ -57,21 +62,36 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     const width = this.getComponentWidth();
+
+    const destination = config.destination;
+    const { images, latitude, longitude } = this.state.selected;
+    const origin = { latitude, longitude };
+
     return (
       <div className={classes.root}>
         <Grid container spacing={spacing}>
           <Grid item xs={12} md={6}>
             <Grid container spacing={spacing}>
               <Grid item xs={12}>
-                <Paper className={classes.paper}>Graph</Paper>
+                <Paper className={classes.paper}>
+                  <div width={width}>Graph</div>
+                </Paper>
               </Grid>
               <Grid item xs={12}>
-                <Paper className={classes.paper}>Map</Paper>
+                <Paper className={classes.paper}>
+                  <div origin={origin} destination={destination} width={width}>
+                    Map
+                  </div>
+                </Paper>
               </Grid>
             </Grid>
           </Grid>
           <Grid item xs={12} md={6}>
-            <Paper className={classes.paper}>Images</Paper>
+            <Paper className={classes.paper}>
+              <div images={images} width={width}>
+                Images
+              </div>
+            </Paper>
           </Grid>
         </Grid>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import data from './data.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
 registerServiceWorker();
